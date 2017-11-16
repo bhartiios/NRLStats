04024db9b68c72ccda0f435798f2442e41f5f2fa
Bharti Sharma <bhartisharma@Bhartis-MacBook-Pro.local>
2017-11-17 10:27:24 +1100

    Files added

---
+++ NRLStats/Networking/AppConstant.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstant.swift
+//  WeatherApp
+//
+//  Created by Bharti Sharma on 01/11/17.
+//  Copyright © 2017 Bharti Sharma. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    
+    static let statsUrl = "https://statsapi.foxsports.com.au/3.0/api/sports/league/matches/NRL20172101/topplayerstats.json;type=fantasy_points;type=tackles;type=runs;type=run_metres?limit=5&userkey=A00239D3-45F6-4A0A-810C-54A347F144C2"
+    
+    static let imageURL = "https://media.foxsports.com.au/match-centre/includes/images/headshots/landscape/nrl/"
+}
